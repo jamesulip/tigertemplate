924bd0b96d81717edda8106bd2744cf3187c4e0e
jamesulip <ulip.james@gmail.com>
2020-11-25 22:44:27 +0800

    menu item

---
+++ src/components/menus/menu.vue
@@ -0,0 +1,11 @@
+<template>
+    <li class="nav-item">
+        <a href="#" class="nav-link">
+          <i class="nav-icon fas fa-th"></i>
+          <p>
+            Simple Link
+            <span class="right badge badge-danger">New</span>
+          </p>
+        </a>
+      </li>
+</template>
