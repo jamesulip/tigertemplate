26fce3be4ef45ec914e0a854a36de56fbfc54320
jamesulip <ulip.james@gmail.com>
2020-11-25 22:41:39 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,19 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <body class="sidebar-mini layout-fixed">
+    <home/>
+  </body>  
 </template>
-
+<style src="../node_modules/@fortawesome/fontawesome-free/css/all.min.css"></style>
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  @import "admin-lte";
+  // @import '~@fortawesome/fontawesome-free/css/all.min.css';
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
+<script>
+
+export default {
+  components:{
+    home:()=>import('./views/Home')
+  }   
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,18 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+
+
+try {
+  window.Popper = require('popper.js').default;
+  window.$ = window.jQuery = require('jquery');
+  require('admin-lte'); // Include AdminLTE
+} catch (e) {
+  console.log('error',e);
+}
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 Vue.config.productionTip = false;
 
